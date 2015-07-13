0c1b0da7b576d9ffe971ae2ba04333ef341393ae
thomas.nairn <thomas.nairn@thinkinnovate.co.uk>
2015-07-13 16:11:15 +0100

    laravel build test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
 @section('main')
     <div class="section light-blue white-text no-pad-bot">
     <div class="container">
-        <h1>VotePlay</h1>
+        <h1>Vote<strong class="lime-text text-accent-3">Play</strong></h1>
     </div>
     <div class="row">
          <div class="col s12 center">
@@ -19,19 +19,19 @@
     <div class="container">
     <div class="row">
      <div class="col m4 s12 center">
-        <i class="material-icons large">play_circle_outline</i>
+        <i class="material-icons large glow">play_circle_outline</i>
         <p class="light-blue-text text-accent-1">
           <strong class="light-blue-text text-lighten-5">Gather</strong> music from the sources you love: YouTube, Soundcloud...
         </p>
      </div>
      <div class="col m4 s12 center">
-        <i class="material-icons large">question_answer</i>
+        <i class="material-icons large glow">question_answer</i>
         <p class="light-blue-text text-accent-1">
           <strong class="light-blue-text text-lighten-5">Chat</strong> with people who like the same music as you!
         </p>
      </div>
      <div class="col m4 s12 center">
-        <i class="material-icons large">queue_music</i>
+        <i class="material-icons large glow">queue_music</i>
         <p class="light-blue-text text-accent-1">
             <strong class="light-blue-text text-lighten-5">Discover</strong> new music, loved by others in the same room!
         </p>
